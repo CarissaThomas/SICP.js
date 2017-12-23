a34babaf4098499473708792aea744d3975d0efc
Carissa Thomas <CarissaThomas@users.noreply.github.com>
2017-12-23 12:49:57 -0800

    Add files via upload

---
+++ Structure and Interpretation of Computer Programs.js
@@ -0,0 +1,70 @@
+//fibonacci as recurisve problem 
+function fibonacciRecursion(n) {
+    if (n = 0) {
+        n = 1;
+        fibonacciRecursion(n)
+    } else {
+        var fibParam1 = n - 1;
+        var fib2Parm2 = n - 2;
+        var fib1 = fibonacciRecursion(fibParam1) + fibonacciRecursion(fibParam2);
+        console.log(fib1);
+    }
+}
+
+//fibonacci as iterative problem
+var count = 0;
+
+function fibonacciIteration(a, b, count) {
+    if (a = 0) {
+        count++;
+        return b;
+    } else {
+        var fib1 = a + b;
+        a = count - 1;
+        count++;
+        console.log(a, fib1, count);
+        fibonacciIteration(a, fib1, count);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
