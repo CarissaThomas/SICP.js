baacac00fcdafb81d92b98accd10253aac5dd625
Carissa Thomas <CarissaThomas@users.noreply.github.com>
2018-01-09 14:33:55 -0800

    Update 1.32.js

--- 1.32.js
+++ 1.32.js
@@ -3,7 +3,7 @@ function product(term,a,next,b){
    if (a > b)
         return 1;
    else return term(a) * 
-               multiplication(term, next(a),next,b);
+               product(term, next(a),next,b);
 }
 
 //these are defined to compute pi
@@ -24,7 +24,7 @@ function even(n) {
 }
 
 function multiply_products(a,b) {
-   return product(pi-term 1 next 6) * 4;//calls main function with params and selected functions
+   return product(pi-term,a,next,b) * 4;//calls main function with params and selected functions
 }
 
 multiply_products(6,7);
@@ -32,27 +32,12 @@ multiply_products(6,7);
 
 //Iterative Version 
 function product(term,a,next,b){
-  function iter(a, result){//is our counter 
-    if (a > b){
-       return 1;
+  function iter(a, result){//result is our counter 
+    if (a > b){//a acts as our counter 
+       return result;
     } else {
-      return term(a) * multiplication(term, next(a),next,b);
-    }
+      return iter(next(a), (term(a) * result));//next adds one to number (iterator), term(a) * result is our  
+    }                                         //previously calculated pi to be mulitplied by our newly returned result 
   }
-  return iter(a,0);//counter starts at 0
+  return iter(a,1);//result starts at 1
 }
-
-
-function sum(term,a,next,b){
-  function iter(a,result){
-    if(a > b){//if we are out of our range, function is complete, return the result 
-      console.log('The result:', result);
-      return result;
-    } else {//increment a update the point in the range, add our previous result to the cube of our previous number 
-      return iter(next(a),result + term(a));//next = inc, term = cube
-    }
-  }
-  return iter(a, 0);//result starts as 0
-}
-
-
